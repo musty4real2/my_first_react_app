f837f39dc7bcbb252b199164052d67295d314235
musty4real2 <musty4real2@gmail.com>
2024-04-28 21:21:29 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css'; 
+import { useState, useEffect } from 'react';
+import './App.css';
 
 // const Person = (props) => {
 //   return (
@@ -15,13 +15,16 @@ import './App.css';
 <Person name='Summer' lastName='Benjamin' age='25' />>
 <Person name='Sadiq' lastName='Maaz' age='22'/> */}
 const App = () => {
-  const [counter,setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    alert("you've changed the counter to "+ counter);
+  },[counter])
   return (
     <div className="App">
-      <button onClick={ () => setCounter((prevCount)=>prevCount -1)}>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
       <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=>prevCount + 1)}>+</button>
-    
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+
     </div>
   );
 }
